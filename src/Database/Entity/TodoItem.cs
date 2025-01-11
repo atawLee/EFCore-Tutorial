fbd69211fa6b642f5018d54edb50d0bcade3d901
jonghoon <jonghoon.1738@gmail.com>
2025-01-11 12:31:10 +0900

    마이그레이션 까지 완료

---
+++ src/Database/Entity/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database.Entity;
+
+public class TodoItem
+{
+    [Key]
+    public int Id { get; set; } // Primary Key
+    public string Title { get; set; } = string.Empty; // 제목
+    public bool IsCompleted { get; set; } = false; // 완료 여부
+    public DateTime? DueDate { get; set; } // 마감일 (null 가능)
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // 생성 날짜
+}
---
+++ src/Migrator/TodoDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Database.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Migrator;
+
+public class TodoDbContextFactory : IDesignTimeDbContextFactory<TodoDbContext>
+{
+    public TodoDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<TodoDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseSqlite(connectionString
+            , b=> b.MigrationsAssembly("Migrator"));
+
+        return new TodoDbContext(builder.Options);
+    }
+}
