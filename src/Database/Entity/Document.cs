58c94e84eafc6af5ad15e678b649ea57475a3ed8
jonghoon <jonghoon.1738@gmail.com>
2025-02-02 22:52:21 +0900

    add table mapping

---
+++ src/Database/Entity/Document.cs
@@ -0,0 +1,26 @@
+﻿namespace Database.Entity;
+
+public class DocumentBase
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
+
+public class GeneralDocumentBase : DocumentBase
+{
+    public string Content { get; set; }
+}
+
+public class ContractDocumentBase : DocumentBase
+{
+    public string ContractorName { get; set; }
+    public DateTime ExpirationDate { get; set; }
+}
+
+public class TechnicalDocumentBase : DocumentBase
+{
+    public string TechnologyUsed { get; set; }
+    public string Version { get; set; }
+}
