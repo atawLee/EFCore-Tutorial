816093106748a9520c25cff7e906a8908ce23396
jonghoon <jonghoon.1738@gmail.com>
2025-03-10 22:12:55 +0900

    use database

--- src/Application/Program.cs
+++ src/Application/Program.cs
@@ -21,13 +21,20 @@ var context= service.GetRequiredService<ShopDbContext>();
 
 context.Payments.AddAsync(new CreditCardPayment
 {
-    Amount = 3000,
+    Amount = 3001,
     PaymentDate = DateTimeOffset.UtcNow,
     Status = "Completed",
     CardNumber = "0000-0000-0000-1234",
     CardHolder = "John Doe",
     ExpirationDate = "12/23"
 });
+
+var list = await context.Payments.OfType<CreditCardPayment>().ToListAsync();
+foreach (var creditCardPayment in list)
+{
+    Console.WriteLine($"Id: {creditCardPayment.Id}, Amount: {creditCardPayment.Amount}, PaymentDate: {creditCardPayment.PaymentDate}, Status: {creditCardPayment.Status}, CardNumber: {creditCardPayment.CardNumber}, CardHolder: {creditCardPayment.CardHolder}, ExpirationDate: {creditCardPayment.ExpirationDate}");
+}
+
 await context.SaveChangesAsync();
 
 Console.ReadLine();
